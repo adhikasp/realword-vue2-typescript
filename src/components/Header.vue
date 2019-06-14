b0f80e331fe109a92fa73ce186339815eba45f99
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2019-06-14 23:32:53 +0700

    Create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-light" id="nav">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">conduit</a>
+      <ul class="nav navbar-nav pull-xs-right">
+        <li class="nav-item">
+          <!-- Add "active" class when you're on that page" -->
+          <router-link to="" class="nav-link active">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="" class="nav-link">
+            <i class="ion-compose"></i>&nbsp;New Post
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="" class="nav-link">
+            <i class="ion-gear-a"></i>&nbsp;Settings
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="" class="nav-link">Sign up</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Header extends Vue {
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <span>This is home page</span>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Header from '@/components/Header.vue';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component
 export default class Home extends Vue {}
 </script>
